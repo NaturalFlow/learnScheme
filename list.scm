b60bfd7c241c3fbafd3de563d5184a59830a240c
L.Brito <31296430+NaturalFlow@users.noreply.github.com>
2018-04-25 00:30:58 -0700

    Update list.scm

--- list.scm
+++ list.scm
@@ -1,5 +1,11 @@
 (define myList(list "Lucas" "Bernardo" "Brito"))
 
 (print myList)
-(print(car myList)) ;; Extracts the first element
-(print(cdr myList)) ;; Extracts all elements except the first
+(print(car myList))  ;; Extracts the first element
+(print(cdr myList))  ;; Extracts all elements except the first
+
+(define nums(list 0 1 2 3 4 5 6 7 8 9))
+
+(print(length nums)   ;; Size = 10
+(print(reverse nums)  ;; Will print nums in reverse order
+(print(check 10 nums) ;; Check if 10 exist in  nums
