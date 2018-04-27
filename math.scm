f882935ec01f518da2449d31dafdc5c9bbd24cfc
L.Brito <31296430+NaturalFlow@users.noreply.github.com>
2018-04-27 00:34:09 -0700

    Update math.scm

--- math.scm
+++ math.scm
@@ -1,7 +1,7 @@
 ;; Some math Functions
-(define (square x) (* x x))
-(define (abs x) (if (< x 0) (- x) x))
-(define (average x y) (/(+ x y) 2))
+(define (square x) (* x x))           ;; (define square (lambda (x) (* x x)))
+(define (abs x) (if (< x 0) (- x) x)) ;; (define abs (lambda (x) (if (< x 0) (- x) x))
+(define (average x y) (/(+ x y) 2))   ;; (define average (lambda (x y) (/ (+ x y) 2))
 ;; Examples of use
 (print(square 10))
 
